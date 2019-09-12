fe41b94d3a1ed48730343307916774f20f4a18c6
Oliver Paschke <oliver.paschke@bytro.com>
2019-09-12 15:50:16 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,83 @@
+module.exports = {
+    extends: ['airbnb-base', 'prettier'],
+    plugins: [
+        // 'import',
+        'prettier',
+    ],
+    parser: 'babel-eslint',
+    parserOptions: {
+        ecmaVersion: 8,
+    },
+    env: {
+        es6: true,
+        browser: true,
+        amd: true,
+        commonjs: true,
+    },
+    globals: {
+        BYTRO: true,
+    },
+    settings: {
+        // 'import/resolver': {
+        //     node: {
+        //         moduleDirectory: ['hup/src/main/js', 'hup/src/main', 'node_modules'],
+        //     },
+        // },
+    },
+    rules: {
+        'global-require': 'off',
+        'no-console': 'off',
+        'no-continue': 'off',
+        'no-param-reassign': 'off',
+        'no-plusplus': 'off',
+        'no-restricted-syntax': [
+            'error',
+            {
+                selector: 'ForOfStatement',
+                message:
+                    'iterators/generators require regenerator-runtime, which is too heavyweight for this guide to allow them. Separately, loops should be avoided in favor of array iterations.',
+            },
+            {
+                selector: 'LabeledStatement',
+                message:
+                    'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.',
+            },
+            {
+                selector: 'WithStatement',
+                message:
+                    '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.',
+            },
+        ],
+        'no-shadow': 'off',
+        'no-underscore-dangle': 'off',
+        'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+        'sort-imports': [
+            'error',
+            {
+                'ignoreCase': false,
+                'ignoreDeclarationSort': true,
+                'ignoreMemberSort': false,
+                'memberSyntaxSortOrder': ['none', 'all', 'single', 'multiple'],
+            },
+        ],
+        'import/no-default-export': ['error'],
+        'import/no-unresolved': 'off',
+        // 'import/no-unresolved': [
+        //     'error',
+        //     {
+        //         commonjs: true,
+        //         amd: true,
+        //     },
+        // ],
+        'import/no-extraneous-dependencies': [
+            'error',
+            {
+                devDependencies: true,
+                optionalDependencies: false,
+                peerDependencies: false,
+            },
+        ],
+        'import/prefer-default-export': 'off',
+        'prettier/prettier': ['error'],
+    },
+};
