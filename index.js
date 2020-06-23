82ca1fb658bee7d5659facbef3c26f97f5f4c46a
Oliver Paschke <oliver.paschke@bytro.com>
2020-06-23 12:25:53 +0200

    2.4.0 Allow "ignoreRestSiblings" no-unused-vars

--- index.js
+++ index.js
@@ -58,8 +58,8 @@ module.exports = {
         ],
         'no-shadow': 'off',
         'no-underscore-dangle': 'off',
-        '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-        "prefer-destructuring": ["error", { "object": true, "array": false }],
+        '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_', ignoreRestSiblings: true }],
+        'prefer-destructuring': ['error', { object: true, array: false }],
         'sort-imports': 'off',
 
         'import/no-default-export': ['error'],
